71e5e1ecaf916f219d0c29f9b7a7df23fa56bf1e
Fillip J Fry <fry_2018@ukr.net>
2023-03-03 17:35:14 +0200

    JSON convertion added

--- src/main/java/com/goit/fry/App.java
+++ src/main/java/com/goit/fry/App.java
@@ -1,13 +1,22 @@
 package com.goit.fry;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.google.gson.Gson;
+
+public class App {
+    private String name;
+    private String lastName;
+
+    public static void main(String[] args) {
+        App app = new App();
+        Gson gson = new Gson();
+
+        String json = gson.toJson(app);
+        System.out.println(json);
+    }
+
+    public App() {
+
+        name = "Fillip J";
+        lastName = "Fry";
     }
 }
